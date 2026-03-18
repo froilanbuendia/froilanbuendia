c1d2726537360fdf75d1ba52cffa8cf6e49dd40e
froilanbuendia <froilangbuendia@gmail.com>
2026-03-17 23:32:48 -0700

    test for render

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,34 +1,35 @@
 "use client";
-import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import ScrollProgressBar from "./components/ScrollProgressBar/ScrollProgressBar";
-import { useRef } from "react";
+// import Hero from "./components/Hero/Hero";
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+// import ScrollProgressBar from "./components/ScrollProgressBar/ScrollProgressBar";
+// import { useRef } from "react";
 
 export default function Page() {
-  const aboutRef = useRef<HTMLElement>(null);
-  const projectsRef = useRef<HTMLElement>(null);
-  const heroRef = useRef<HTMLElement>(null);
-  const scrollToAbout = () => {
-    aboutRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
-  const scrollToProjects = () => {
-    projectsRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
-  const scrollToHero = () => {
-    heroRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-  };
+  // const aboutRef = useRef<HTMLElement>(null);
+  // const projectsRef = useRef<HTMLElement>(null);
+  // const heroRef = useRef<HTMLElement>(null);
+  // const scrollToAbout = () => {
+  //   aboutRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  // };
+  // const scrollToProjects = () => {
+  //   projectsRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  // };
+  // const scrollToHero = () => {
+  //   heroRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  // };
 
   return (
     <div className="page-container">
-      <ScrollProgressBar />
+      {/* <ScrollProgressBar />
       <Hero
         heroRef={heroRef}
         handleAboutRef={scrollToAbout}
         handleProjectsRef={scrollToProjects}
       />
       <Projects projectsRef={projectsRef} />
-      <About aboutRef={aboutRef} handleHeroRef={scrollToHero} />
+      <About aboutRef={aboutRef} handleHeroRef={scrollToHero} /> */}
+      Hello World
     </div>
   );
 }
